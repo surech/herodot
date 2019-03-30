fe5eba59f4bfbcfe03095ade4faec4d9010464ce
surech <stefan@surech.ch>
2019-03-30 14:09:09 +0100

    stu: Initial Spring Boot-Sceleton with one Controller and an index.html

---
+++ src/main/java/ch/surech/herodot/HerodotApplication.java
@@ -0,0 +1,13 @@
+package ch.surech.herodot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerodotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerodotApplication.class, args);
+    }
+
+}
